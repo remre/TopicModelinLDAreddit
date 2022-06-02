32ee6facb1f16e88ad2fecb34a5047e03979e9a8
Emre <basarremre@outlook.com>
2022-06-02 06:07:53 +0300

    check the speed

--- databaseeditor.py
+++ databaseeditor.py
@@ -2,6 +2,7 @@ import csv
 import psycopg2
 import glob
 from psycopg2 import sql
+from datetime import datetime
 
 
 conn = psycopg2.connect('postgres://oynkonierltdgy:2e6c52a982ebe5d83448dd0f2a0a59115d028e9e6d1d2a394c713953e13c87ba@ec2-107-22-238-112.compute-1.amazonaws.com:5432/da7522agbf56fl')
@@ -12,25 +13,20 @@ cur = conn.cursor()
 
 
 
-# value = [] 
-# {value.append(file.split('\\')[1].split('.')[0]) for file in glob.glob("w_seltexts\*.txt")} 
+value = [] 
+{value.append(file.split('\\')[1].split('.')[0]) for file in glob.glob("w_seltexts\*.txt")} 
 
-b = 'asdasd'
-query = sql.SQL('''
-    insert into use (words)
-    values {}
-''').format(sql.Identifier(b))
 
-cur.execute (query, ('b'))
+# cur.execute(sql.SQL("INSERT INTO {} VALUES (%s)").format(sql.Identifier(c)),(b,))
 
 
 # a = ['zazazaza','sdfsf']
-# cur.execute("CREATE TABLE use(words text)")
+
 # cur.execute(f"INSERT INTO use (words) VALUES  {a}")
 
-cur.execute('SELECT * FROM use')
-for i in cur.fetchall():
-    print(i)
+# cur.execute('SELECT * FROM use')
+# for i in cur.fetchall():
+#     print(i)
 
 # print(value)
 # cur.execute("""CREATE TABLE use(
@@ -40,26 +36,34 @@ for i in cur.fetchall():
 
 # conn.commit()
 
-# for lang in value:
-# # #     # id integer PRIMARY KEY,
-# # #     # print(lang)
+for lang in value:
+# #     # id integer PRIMARY KEY,
+# #     # print(lang)
 #     cur.execute(f"""CREATE TABLE {lang}(
 #         words text
 # )
 #         """)
-    # with open('w_seltexts/{}.txt'.format(lang), 'r', errors='pass') as f:
-    #     lines = f.readlines()
-    #     for line in lines:
-    #         if line != '\n':
+#     conn.commit()
+    start_time = datetime.now()
+    with open('w_seltexts/{}.txt'.format(lang), 'r', errors='replace') as f:
+        lines = f.readlines()
+        for line in lines:
+            if line != '\n':
+                cur.execute(sql.SQL("INSERT INTO {}(words) VALUES (%s)").format(sql.Identifier(lang)),(line,))
+                end_time = datetime.now()  #put end time where u want to cunt 
+print('Duration: {}'.format(end_time - start_time))
+                # cur.execute(sql.SQL('SELECT * FROM {}').format(sql.Identifier(lang)))
+                # for i in cur.fetchone():
+                #     print(i)
                 # insert_query = "INSERT INTO {}(words) VALUES {}".format(lang,line)
-    #             cur.execute(insert_query)
+                # cur.execute(insert_query)
                 
 #                 # cur.execute('SELECT * FROM de')
 #                 cur.execute("INSERT INTO {}(text) VALUES {}".format(lang, line))
 #             # cur.execute(f"INSERT INTO %s VALUES %s", (lang, line))
 
 
-# conn.commit()
+conn.commit()
 
 
 # for lang in value:
